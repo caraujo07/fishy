7bff21b7a0c58c77944045faf3dba3e8c864aa4a
Cesar Oliveira dos Santos Araujo <caraujo@jc.com.br>
2019-09-16 12:50:14 -0300

    Testando cadastro de usuários com uma rota POST

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
+import User from "./app/models/User";
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-  return res.json({ message: "Hello, world!" });
+routes.get("/", async (req, res) => {
+  const user = await User.create({
+    name: "César O. Araújo",
+    email: "caraujo@jc.com.br",
+    password_hash: "654asd321"
+  });
+  return res.json(user);
 });
 
 export default routes;
